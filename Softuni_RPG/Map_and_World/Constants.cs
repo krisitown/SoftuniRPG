626841b93ddb84b531ebddb40f69d9ead1bdb0db
PrPetkov <prpetkov@dir.bg>
2015-12-10 10:35:14 +0200

    Added Constants class

---
+++ Softuni_RPG/Map_and_World/Constants.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Softuni_RPG.Map_and_World
+{
+    class Constants
+    {
+
+        public static readonly double maxPlayerHealth = 100;
+        public static readonly double mapRows = 10;
+        public static readonly double mapCols = 10;
+
+    }
+}
